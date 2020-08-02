adca92d5f2e1a2686f1717ff2a6c18cca493c2ce
Julian <julian@jull.dev>
2020-08-02 13:27:10 +0200

    Implement std::ops for Point

--- src/point.rs
+++ src/point.rs
@@ -1,3 +1,8 @@
+use std::ops::Add;
+use std::ops::Mul;
+use std::ops::Rem;
+use std::ops::Sub;
+
 #[derive(Copy, Clone, Debug)]
 pub struct Point {
     pub x: f64,
@@ -10,48 +15,88 @@ impl Point {
         Self { x, y, z }
     }
 
-    pub fn mul_scalar(&self, value: f64) -> Self {
-        Self::new(self.x * value, self.y * value, self.z * value)
+    pub fn length(self) -> f64 {
+        1.0 / self.dot(self).powf(-0.5)
     }
 
-    pub fn add(&self, other: Self) -> Self {
-        Self::new(self.x + other.x, self.y + other.y, self.z + other.z)
-    }
-
-    pub fn add_scalar(&self, value: f64) -> Self {
-        Self::new(self.x + value, self.y + value, self.z + value)
-    }
-
-    pub fn sub(&self, other: Self) -> Self {
-        Self::new(self.x - other.x, self.y - other.y, self.z - other.z)
-    }
-
-    pub fn length(&self) -> f64 {
-        1.0 / self.dot(*self).powf(-0.5)
-    }
-
-    pub fn dot(&self, other: Self) -> f64 {
+    pub fn dot(self, other: Self) -> f64 {
         self.x * other.x + self.y * other.y + self.z * other.z
     }
 
-    pub fn normalize(&self) -> Self {
-        self.mul_scalar(self.dot(*self).powf(-0.5))
+    pub fn normalize(self) -> Self {
+        self * self.dot(self).powf(-0.5)
     }
 
-    pub fn cross(&self, other: Self) -> Self {
-        Self::new(self.y * other.z, self.z * other.x, self.x * other.y).sub(Self::new(
-            self.z * other.y,
-            self.x * other.z,
-            self.y * other.x,
-        ))
-    }
-
-    pub fn r#mod(&self, n: f64) -> Self {
-        Self::new(r#mod(self.x, n), r#mod(self.y, n), r#mod(self.z, n))
+    pub fn cross(self, other: Self) -> Self {
+        Point::new(self.y * other.z, self.z * other.x, self.x * other.y)
+            - Point::new(self.z * other.y, self.x * other.z, self.y * other.x)
     }
 
     pub fn abs(&self) -> Self {
-        Self::new(self.x.abs(), self.y.abs(), self.z.abs())
+        Point::new(self.x.abs(), self.y.abs(), self.z.abs())
+    }
+}
+
+#[deprecated]
+impl Point {
+    pub fn mul_scalar(self, value: f64) -> Self {
+        self * value
+    }
+
+    pub fn add(self, other: Self) -> Self {
+        self + other
+    }
+
+    pub fn add_scalar(self, value: f64) -> Self {
+        self + value
+    }
+
+    pub fn sub(self, other: Self) -> Self {
+        self - other
+    }
+
+    pub fn r#mod(self, n: f64) -> Self {
+        self % n
+    }
+}
+
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Point::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
+    }
+}
+
+impl Add<f64> for Point {
+    type Output = Self;
+
+    fn add(self, rhs: f64) -> Self {
+        Point::new(self.x + rhs, self.y + rhs, self.z + rhs)
+    }
+}
+
+impl Sub for Point {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        Point::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
+    }
+}
+
+impl Mul<f64> for Point {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self {
+        Point::new(self.x * rhs, self.y * rhs, self.z * rhs)
+    }
+}
+
+impl Rem<f64> for Point {
+    type Output = Self;
+
+    fn rem(self, rhs: f64) -> Self {
+        Point::new(r#mod(self.x, rhs), r#mod(self.y, rhs), r#mod(self.z, rhs))
     }
 }
 
